ba6046e17318ab43be1a77cf210538bb65d9c681
Alex Hipolito <alexheepo@gmail.com>
2021-04-05 16:46:27 +1000

    Added more details about the project in script.js. Added showPage function. Call showPage function for first page for now.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+Author: Alex Hipolito
+GitHub: @alexhippo
 */
 
 
@@ -17,8 +19,30 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+let itemsPerPage = 9;
+function showPage(list, page) {
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   let studentList = document.querySelector('ul.student-list');
+   studentList.innerHTML = ``;
 
-
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         studentList.innerHTML += `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src=${list[i].picture.large} alt="Profile Picture of ${list[i].name.first} ${list[i].name.last}">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">${list[i].registered.date}</span>
+               </div>
+         </li>
+         `
+      }
+   }
+}
 
 /*
 Create the `addPagination` function
@@ -28,3 +52,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
